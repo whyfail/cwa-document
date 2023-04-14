e849da9afe8e05436480c9e96551dc0039338dec
吴磊 <wulei0407@163.com>
2023-04-14 18:57:25 +0800

    [更新]:更新插图

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,17 +5,17 @@ import styles from "./styles.module.css"
 const FeatureList = [
   {
     title: "使用简单",
-    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+    Svg: require("@site/static/img/undraw_code.svg").default,
     description: <>在线上验证过，完善配置，快速构建模板，方便实用。无需繁琐配置，专注业务开发，提高效率。</>,
   },
   {
     title: "专注于重要的事情",
-    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    Svg: require("@site/static/img/undraw_vue.svg").default,
     description: <>无需底层配置，专注业务逻辑。脚手架处理完善，快速开发，提高效率，专注核心业务。</>,
   },
   {
     title: "支持React、Vue3",
-    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    Svg: require("@site/static/img/undraw_react.svg").default,
     description: <>提供React和Vue3模板，含全家桶配置。根据需求选择模板，快速搭建项目，专注业务开发。</>,
   },
 ]
